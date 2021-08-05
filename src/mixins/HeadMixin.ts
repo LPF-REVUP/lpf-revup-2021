1c3c82aa19f6913aba9acce139ed225c567a92ea
mochan_tk <takashi1.kawamoto@gmail.com>
2021-08-05 21:27:40 +0900

    change conference name

--- src/mixins/HeadMixin.ts
+++ src/mixins/HeadMixin.ts
@@ -12,12 +12,12 @@ export default class HeadMixin extends Vue {
   public head(): MetaInfo {
     const info = this.headInfo()
 
-    const siteName: string = 'LPF REV UP 2020'
+    const siteName: string = 'LINE Developers Community REV UP 2021'
     const title: string = info.title ? `${info.title} | ${siteName}` : siteName
     const description: string = info.description
       ? // HTML タグを除去
         info.description.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
-      : 'LPF REV UP 2020は普段LINEのAPIに関する勉強会や情報交換を行っている東京、関西、九州のコミュニティが合同で主催するカンファレンスです。2020のテーマは「開発者と共に。ユーザーを支えるLINEプラットフォーム」。'
+      : 'LINE Developers Community REV UP 2021は普段LINEのAPIに関する勉強会や情報交換を行っている東京、関西、九州のコミュニティが合同で主催するカンファレンスです。2020のテーマは「開発者と共に。ユーザーを支えるLINEプラットフォーム」。'
 
     const baseUrl: string = process.env.BASE_URL || ''
     const thisUrl: string = `${baseUrl}${this.$route.path}`
