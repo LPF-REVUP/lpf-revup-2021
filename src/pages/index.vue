7d6085df15821738e69a2e29a4de384f1fc3563d
mochan_tk <takashi1.kawamoto@gmail.com>
2021-08-06 20:45:59 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,7 +23,7 @@
       v-container.py-0
         v-layout(justify-center)
           div
-            a(href="https://book.mynavi.jp/ec/products/detail/id=117310" target="_blank") REV UP 2021 セッション公募開始！
+            a(href="https://forms.gle/3vhEeHxTTm2KztdM7" target="_blank") REV UP 2021 セッション公募開始！
             | が50名様に当たる
             a(href="https://twitter.com/linedc_jp/status/1323918060839866369" target="_blank") Twitterキャンペーン
             | 実施中です！
