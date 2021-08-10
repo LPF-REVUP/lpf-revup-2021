20cdf3987e5636a6a02305203da23bbda9df6936
mochan_tk <takashi1.kawamoto@gmail.com>
2021-08-10 18:15:09 +0900

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -67,13 +67,8 @@
           v-col.mx-auto(cols="12" md="6")
             p.body-2
               | 視聴はオンラインです。是非お気軽にご参加下さい。
-            p.body-2
-              | 下記は昨年開催されたREV UP2020年の模様です。
               br
-              | （画像をクリックでREV UP 2020 公式サイトに飛びます）
-              br
-        a(href="https://revup.jp" target="_blank")
-          v-img(src="/revup2020.png")
+              a(href="https://revup.jp" target="_blank") REVup 2020はこちら
       //- Speakers
       //-v-row.section-header(id="speakers" cols="12")
       //-  div.mb-4
