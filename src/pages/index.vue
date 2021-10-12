611acb77a90ec59e1a7cd22d7a5576141f97df84
mochan_tk <takashi1.kawamoto@gmail.com>
2021-10-12 08:07:37 +0000

    modify ask the speaker

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -134,7 +134,7 @@
               v-col.font-weight-bold(cols="12" md="3")
                 | Zoom URL（14:00開始)
               v-col(cols="12" md="9")
-                a(href="https://us02web.zoom.us/j/82516975307?pwd=QkgyT0l3YWthVjBzMTBWcDgvWnZFZz09" target="_blank") Ask The Speaker & 雑談部屋
+                | 終了いたしました、ご参加ありがとうございました。
       //- Speakers
       v-row.section-header(id="speakers" cols="12")
         div.mb-4
